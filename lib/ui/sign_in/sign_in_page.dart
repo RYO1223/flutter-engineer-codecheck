a7cae459e5f32d1205d78a0167d720a58ed77c1a
RYO1223 <kuwanor0811@gmail.com>
2023-11-19 17:40:45 +0900

    style: サインインページにデザインを適用

--- lib/ui/sign_in/sign_in_page.dart
+++ lib/ui/sign_in/sign_in_page.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_engineer_codecheck/ui/app_router.dart';
 import 'package:flutter_engineer_codecheck/view_model/user/user_view_model.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sign_in_button/sign_in_button.dart';
 
 class SignInPage extends ConsumerStatefulWidget {
   const SignInPage({super.key});
@@ -15,21 +16,43 @@ class _SignInPageState extends ConsumerState<SignInPage> {
   Widget build(BuildContext context) {
     final userViewModel = ref.watch(userViewModelProvider.notifier);
     return Scaffold(
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          ElevatedButton(
-            onPressed: () async {
-              await userViewModel.signInWithGithub();
-              if (!mounted) {
-                return;
-              }
-              const HomeRoute().go(context);
-            },
-            child: const Text('Sign in with GitHub'),
-          ),
-        ],
+      body: Align(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Sign In', style: Theme.of(context).textTheme.displayMedium),
+            const SizedBox(height: 100),
+            SizedBox(
+              height: 400,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  _githubButton(context, userViewModel),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
+    );
+  }
+
+  SignInButton _githubButton(
+    BuildContext context,
+    UserViewModel userViewModel,
+  ) {
+    return SignInButton(
+      Buttons.gitHub,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(100)),
+      ),
+      onPressed: () async {
+        await userViewModel.signInWithGithub();
+        if (!mounted) {
+          return;
+        }
+        const HomeRoute().go(context);
+      },
     );
   }
 }
